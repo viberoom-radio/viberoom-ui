bad61b7a438476bc68190debdbaf7ba9c3f3a68f
Konstantin Zhitkov <kzhitkov@adsterra.com>
2023-04-28 19:29:34 +0300

    (structure): move rich text component to shared folder

---
+++ rich-text/rich-text.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PortableText } from '@portabletext/react';
+import { Link } from 'components/Link';
+import { Quote } from 'shared/ui/quote';
+import { Props, Components } from './types';
+
+const components: Components = {
+  marks: {
+    link: ({ children, value }) => <Link href={value.href}>{children}</Link>,
+  },
+  block: {
+    blockquote: ({ children }) => <Quote>{children}</Quote>,
+  },
+};
+
+export const RichText = ({ value = [] }: Props) => (
+  <PortableText value={value} components={components} />
+);
---
+++ rich-text/types.ts
@@ -0,0 +1,7 @@
+import { PortableTextProps } from '@portabletext/react';
+
+export type Props = {
+  value: PortableTextProps['value'];
+};
+
+export type Components = PortableTextProps['components'];
