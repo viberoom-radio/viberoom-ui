291b8619b0c1fd670f64f36e133b56f26cde14b7
Konstantin Zhitkov <kzhitkov@adsterra.com>
2023-04-28 19:28:22 +0300

    (structure): move quote component to shared folder

---
+++ quote/quote.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import * as Styled from './styles';
+
+export const Quote = ({ children }) => <Styled.Quote>{children}</Styled.Quote>;
---
+++ quote/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { colors } from 'constants/colors';
+
+export const Quote = styled.blockquote`
+  margin: 0;
+  padding: 0 1rem;
+  border-left: 2px solid ${colors.bg.secondary};
+`;
