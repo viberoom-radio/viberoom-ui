ed7fe0d7aa16b6b19790ac468780953d593652a9
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2023-05-04 01:33:47 +0300

    (docs): add rich text story

---
+++ rich-text/rich-text.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RichText } from './rich-text';
+import { fixture } from './__mocks__/constants';
+
+const meta: Meta<typeof RichText> = {
+  title: 'Shared/RichText',
+  component: RichText,
+  tags: ['autodocs'],
+  args: {
+    value: fixture,
+  },
+  render({ ...props }) {
+    return <RichText {...props} />;
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Image>;
+
+export const Default: Story = {};
+
+export const WithText: Story = {};
