99ea8d34ab452f4dedc8e0326df28dcabdcdb79d
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2023-05-03 10:27:39 +0300

    (docs): add input story

---
+++ input/input.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from './input';
+
+const meta: Meta<typeof Input> = {
+  title: 'Shared/Input',
+  component: Input,
+  tags: ['autodocs'],
+  render({ ...props }) {
+    return <Input {...props} />;
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {};
+
+export const Filled: Story = {
+  args: {
+    defaultValue: 'Value',
+  },
+};
