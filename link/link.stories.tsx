ea0085cc106e7879cf9878672470a8968a303888
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2023-05-04 01:21:24 +0300

    (docs): add link story

---
+++ link/link.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Link } from './link';
+
+const meta: Meta<typeof Link> = {
+  title: 'Shared/Link',
+  component: Link,
+  tags: ['autodocs'],
+  args: {
+    children: 'Link',
+    href: '#',
+  },
+  render({ ...props }) {
+    return <Link {...props} />;
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Link>;
+
+export const Default: Story = {};
+
+export const External: Story = {
+  args: {
+    href: 'https://example.com',
+  },
+};
+
+export const Internal: Story = {
+  args: {
+    href: '/#',
+  },
+};
