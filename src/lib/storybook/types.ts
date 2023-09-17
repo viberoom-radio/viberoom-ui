d3d8dc20e2e986ec09d7e45dd9fde7a1fa9144f4
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2023-09-17 12:05:41 +0300

    fix: format files

--- src/lib/storybook/types.ts
+++ src/lib/storybook/types.ts
@@ -1,6 +1,6 @@
-import type { ChangeEvent } from 'react'
+import type { ChangeEvent } from 'react';
 
 export type Args = {
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
-  value?: number | string
-}
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value?: number | string;
+};
--- src/lib/storybook/with-controlled-state.tsx
+++ src/lib/storybook/with-controlled-state.tsx
@@ -1,33 +1,33 @@
-import type { ChangeEvent } from 'react'
-import { useArgs } from '@storybook/preview-api'
-import type { StoryContext, StoryFn } from '@storybook/react'
-import type { Args } from './types'
+import type { ChangeEvent } from 'react';
+import { useArgs } from '@storybook/preview-api';
+import type { StoryContext, StoryFn } from '@storybook/react';
+import type { Args } from './types';
 
 export const WithControlledState = (
   Story: StoryFn,
-  ctx: StoryContext<Args>,
+  ctx: StoryContext<Args>
 ) => {
-  const [, setArgs] = useArgs<Args>()
+  const [, setArgs] = useArgs<Args>();
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target
+    const { value } = event.target;
     // Call the provided callback
     // This is used for the Actions tab
-    ctx.args.onChange?.(event)
+    ctx.args.onChange?.(event);
 
     // Check if the component is controlled
-    if (typeof ctx.args.value === undefined) {
-      return
+    if (typeof ctx.args.value === 'undefined') {
+      return;
     }
 
     // Update the arg in Storybook
     if (typeof ctx.args.value === 'number') {
-      setArgs({ value: Number(value) })
+      setArgs({ value: Number(value) });
     }
 
-    setArgs({ value })
-  }
+    setArgs({ value });
+  };
 
   // Forward all args and overwrite onValueChange
-  return <Story args={{ ...ctx.args, onChange }} />
-}
+  return <Story args={{ ...ctx.args, onChange }} />;
+};
